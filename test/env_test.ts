0f08412ee823dca426ca99c67090cf38e3631bca
DinkDev <dinkhome@hotmail.com>
2019-05-04 12:13:18 -0400

    Added nearley parser engine and moo lexer.  Added mocha with chai and a dummy unit test.

---
+++ test/env_test.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+
+describe("chai", () => {
+  it("can use expect in a test", () => {
+    // tslint:disable-next-line:typedef
+    const s = new Array<number>();
+    expect(s.length).to.equal(0);
+  });
+});
